486b51f14e4b2c9131dd7214eafab1e2e1edc4e5
Wouter van den Dries <woutervddries@live.nl>
2023-12-24 23:25:50 +0000

    Add post and comment model including meta class and __str__ function

--- goodnewsapp/models.py
+++ goodnewsapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Pusblished"))
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_post")
+    edited_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name="post_likes", blank=True)
+    reported = models.BooleanField(default=False)
+    reports = models.ManyToManyField(User, related_name="post_report_count", blank=True)
+        
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.title} by {self.user}'
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post_comments")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name="comment_likes", blank=True)
+    reported = models.BooleanField(default=False)
+    reports = models.ManyToManyField(User, related_name="post_reports_count", blank=True)
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return f'Comment {self.body} by {self.user}'
+
+
+
