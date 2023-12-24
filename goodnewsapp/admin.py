f2d26dee9acbe095074150995957b758eabbae00
Wouter van den Dries <woutervddries@live.nl>
2023-12-24 23:38:54 +0000

    Run migrations for summernote

--- goodnewsapp/admin.py
+++ goodnewsapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'reported')
+    list_display = ('title', 'user', 'created_on', 'reported')
+    search_fields = ['title', 'content', 'user']
+    summernote_fields = ('content')
+
+   
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_filter = ('user', 'created_on', 'reported')
+    list_display = ('user', 'body', 'post', 'created_on', 'reported')
+    search_fields = ['user', 'body']
+    summernote_fields= ('body')
