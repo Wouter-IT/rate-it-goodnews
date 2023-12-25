52ba78a376d11366e488832e39cc6d866e3beb12
Wouter van den Dries <woutervddries@live.nl>
2023-12-25 13:53:27 +0000

    Add navbar functionality to base.html, add website colour styling and logo

--- goodnewsapp/views.py
+++ goodnewsapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post, Comment
 
-class PostListView(generic.Listview):
+class PostListView(generic.ListView):
     model = Post
+    template_name = 'index.html'
 
