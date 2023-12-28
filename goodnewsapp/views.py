ff97d81644a4aaa2de51fd07b73810906b31d75a
Wouter van den Dries <woutervddries@live.nl>
2023-12-28 14:30:35 +0000

    Add masonry layout for main post page.

--- goodnewsapp/views.py
+++ goodnewsapp/views.py
@@ -6,5 +6,5 @@ class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 9
+    paginate_by = 24
 
